d94dfcf0add38022effb6c56bc1ca5a9dc1390bd
lizza <liutao_glory@126.com>
2021-06-17 11:49:09 +0800

    react diff

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Diff from './Diff';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Diff />
   </React.StrictMode>,
   document.getElementById('root')
 );
